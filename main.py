54a2ed44583764e22e40d20b4b284b8132565e97
Chandni Patel <chandnir.patel@gmail.com>
2017-06-24 12:59:11 +0200

    Add inital facebook page for user to input credentials

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ class MainPage(webapp2.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
         self.response.out.write(template.render(path, {}))
 
+class Facebook(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates/facebook.html')
+        self.response.out.write(template.render(path, {}))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/facebook', Facebook),
 ], debug=True)
